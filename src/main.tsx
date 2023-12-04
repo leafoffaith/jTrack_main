4123fd9d48c6fe420866b0d97edc5197ddaf5e02
leafoffaith <49914454+leafoffaith@users.noreply.github.com>
2023-12-04 15:58:30 +0530

    Router created

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Home, Learn } from './components'
+import { createBrowserRouter, RouterProvier, } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: 'learn',
+    element: <Learn />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvier router={router} />
   </React.StrictMode>,
 )
